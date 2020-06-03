59840d149df48a322e05ef51a5015bbc8b4d4ced
Ujwalsingh595 <ujwalsingh595@gmail.com>
2020-06-03 11:58:10 +0530

    Updates

---
+++ precache-manifest.754d12fcdedb8841a741a25d7626247d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf23a58b9d8103b5067349740d94d67d",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "150a63318a060594b758",
+    "url": "/react/static/css/main.cc1eb84e.chunk.css"
+  },
+  {
+    "revision": "eba1d56e2af77b83c213",
+    "url": "/react/static/js/2.fa3f3a01.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.fa3f3a01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "150a63318a060594b758",
+    "url": "/react/static/js/main.49062d90.chunk.js"
+  },
+  {
+    "revision": "b95130a0bb723ec6fdd4",
+    "url": "/react/static/js/runtime-main.5d03a746.js"
+  }
+]);
