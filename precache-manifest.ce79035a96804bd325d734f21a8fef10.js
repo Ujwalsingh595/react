14115204fa1356ece1e030fb51c9221894fe7eb0
Ujwalsingh595 <ujwalsingh595@gmail.com>
2020-07-01 17:09:36 +0530

    Updates

---
+++ precache-manifest.ce79035a96804bd325d734f21a8fef10.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00bbbb055b230e8389e25f2bc5527f24",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "7458cb1a01c0c96dda26",
+    "url": "/react/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "ed74365dc923bef6fd6d",
+    "url": "/react/static/css/main.abb70559.chunk.css"
+  },
+  {
+    "revision": "7458cb1a01c0c96dda26",
+    "url": "/react/static/js/2.8c499bda.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/react/static/js/2.8c499bda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed74365dc923bef6fd6d",
+    "url": "/react/static/js/main.5fd58426.chunk.js"
+  },
+  {
+    "revision": "b95130a0bb723ec6fdd4",
+    "url": "/react/static/js/runtime-main.5d03a746.js"
+  },
+  {
+    "revision": "15b2867352edf1196f9ad5c0667fbf6d",
+    "url": "/react/static/media/1.15b28673.jpg"
+  },
+  {
+    "revision": "1ce13a000d6bf61149ee6486a135c85b",
+    "url": "/react/static/media/2.1ce13a00.jpg"
+  },
+  {
+    "revision": "9bf19840058b11ac0e50dcf3f02ec6c8",
+    "url": "/react/static/media/2.9bf19840.png"
+  },
+  {
+    "revision": "2ba33f2c6652d61bd37da0c3fd83f4ea",
+    "url": "/react/static/media/3.2ba33f2c.jpg"
+  },
+  {
+    "revision": "a5879701ad3d611be26729682f154646",
+    "url": "/react/static/media/4.a5879701.jpg"
+  },
+  {
+    "revision": "e65ac1950b5e64237f999011e5ecc251",
+    "url": "/react/static/media/5.e65ac195.jpg"
+  },
+  {
+    "revision": "5488bd762d68297aea16a63fa07ae665",
+    "url": "/react/static/media/6.5488bd76.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ujjushop/precache-manifest.d7f5413fc4824af1db45cdf03cf765aa.js"
+  "/react/precache-manifest.ce79035a96804bd325d734f21a8fef10.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ujjushop/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
